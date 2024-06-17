f61418f73cb1cad2305f0e015d9fb3d560a30333
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-06-18 00:17:17 +0600

    advanced data fatching with SSR

--- src/app/post/page.jsx
+++ src/app/post/page.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { getPosts } from '../services/postApi';
 
-const postPage = () => {
-  return (
-    <div className='h-screen'>
-      post page
+
+
+const postPage = async () => {
+    const postsData = await getPosts();
+    return <div className=''>
+        <h6>All Post</h6>
+        <div className='grid grid-cols-4 gap-4'>
+            {
+                postsData?.slice(0,20)?.map(({title, body, id}) =>(
+                    <div className='border-2 p-6' key={id}>
+                        <h5>Title: {title}</h5>
+                        <h6>Body: {body}</h6>
+                    </div>
+                ))
+            }
+        </div>
     </div>
-  )
 }
 
 export default postPage
