0ce55d62f7ee549ce40ebc5cba066c8c88a13969
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-06-18 06:20:26 +0600

    Caching And Revalidating

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,7 +2,13 @@ import AboutContent from '@/components/AboutContent'
 import React from 'react'
 import { Headland_One } from 'next/font/google'
 
-const headland = Headland_One({weight : ['400'], subsets : ['latin']})
+const headland = Headland_One({ weight: ['400'], subsets: ['latin'] })
+
+const getTime = async () => {
+  const res = await fetch(`http://localhost:3000/time`, { cache: 'no-store' }); //{cache : 'no-store'}, { next: { revalidate: 5 } }
+  const data = await res.json();
+  return data.currentTime;
+}
 
 export const metadata = {
   title: {
@@ -11,9 +17,13 @@ export const metadata = {
   description: "About Page",
   keywords: ["about", "about page"]
 }
-const aboutPage = () => {
+const aboutPage = async () => {
+
+  const currentTime = await getTime();
+
   return (
     <div className={`${headland.className} min-h-screen px-12 py-24`}>
+      <h6 className="text-3xl text-red-400 mt-12">Time: {currentTime}</h6>
       <AboutContent />
     </div>
   )
